a6771da26bf7e33454c69d7ba45ddf1bff8bffd3
Danilo Valério <danvalerio89@gmail.com>
2024-02-26 12:05:33 -0300

    fix: go to google form

--- src/components/faq/Buttons.tsx
+++ src/components/faq/Buttons.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { goToGoogleForm } from "@/utils/go-to-google-form";
 
 export interface ButtonFAQProps {
   firstButton: string;
@@ -9,12 +10,17 @@ export function ButtonFAQ({ firstButton, secondButton }: ButtonFAQProps) {
   return (
     <div className="mb-[86px] mt-[50px] hidden lg:flex lg:justify-center lg:gap-6">
       <Button
+        onClick={() => goToGoogleForm()}
         className="w-auto rounded-full border border-transparent bg-red.500 text-white"
         variant={"white"}
       >
         {firstButton}
       </Button>
-      <Button className="w-auto rounded-full" variant={"white"}>
+      <Button
+        className="w-auto rounded-full"
+        variant={"white"}
+        onClick={() => goToGoogleForm()}
+      >
         {secondButton}
       </Button>
     </div>
